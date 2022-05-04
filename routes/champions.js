4fef6040f7a456fc3ae67789eb037b795a87876f
Haig (Hike) Hovsepian <hhovsepi@gmail.com>
2022-05-04 12:56:52 -0700

    champion routes + more
    made a lot of good progress on the front end

--- routes/champions.js
+++ routes/champions.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("Champion Database");
+    res.render('champions', { text: 'League of Legends Simulator' });
+   })
+   router.get('/shaco', (req, res) => {
+    res.render('shaco', { text: 'League of Legends Simulator' });
    })
 
 router.get('/:champion', (req, res) => {
